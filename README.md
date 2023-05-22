4b0a6a740bb2b45aa3dfb8bf756b5414e7965463
Juan Jzb <jj_extreme93@rocketmail.com>
2023-05-21 19:42:37 -0600

    Updated Description

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Dependent Selects
+# Dependent Selects using Vanilla JS
 
 Shows the implementation of dependant selects (combobox) using vanilla js
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const city = document.getElementById('city')
 
 /**
  * Fill Select Function.
- * @summary This function is used to fill dependent html select elements with information stored in a json file.
+ * @summary This function is used to fill dependent html select elements with information stored in a json file using vanilla js.
  * @param { HTMLSelectElement } select - The id of the select element from the html that we need to fill.
  * @param { Array } items - List of element that we are going to use to fill the select item.
  * @return {undefined} - Function does not return any value.
